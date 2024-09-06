45726e571fb34086418e58730a93e0255fe7e80e
danigil <dgilkarov33@gmail.com>
2024-09-07 00:34:36 +0300

    added artifact output metadata in embedding step

--- model_xray/zenml/pipelines/data_creation/preprocessed_image.py
+++ model_xray/zenml/pipelines/data_creation/preprocessed_image.py
@@ -12,16 +12,16 @@ from model_xray.configs.models import *
 
 @pipeline
 def _preprocessed_image_pipeline(preprocessed_image_lineage: PreprocessedImageLineage):
-    cover_data = fetch_cover_data_step(cover_data_config=preprocessed_image_lineage.cover_data_config)
+    cover_data = fetch_cover_data_step(cover_data_config=preprocessed_image_lineage.cover_data_config.model_dump())
 
     if preprocessed_image_lineage.embed_payload_config != ret_na_val():
-        stego_data = embed_payload_into_cover_data_step(cover_data=cover_data, embed_payload_config=preprocessed_image_lineage.embed_payload_config)
+        stego_data = embed_payload_into_cover_data_step(cover_data=cover_data, embed_payload_config=preprocessed_image_lineage.embed_payload_config.model_dump())
         data = stego_data
     else:
         data = cover_data
 
-    image_rep = create_image_representation_step(data=data, image_rep_config=preprocessed_image_lineage.image_rep_config)
-    preprocessed_image = image_preprocessing_step(image=image_rep, image_preprocess_config=preprocessed_image_lineage.image_preprocess_config)
+    image_rep = create_image_representation_step(data=data, image_rep_config=preprocessed_image_lineage.image_rep_config.model_dump())
+    preprocessed_image = image_preprocessing_step(image=image_rep, image_preprocess_config=preprocessed_image_lineage.image_preprocess_config.model_dump())
 
     return preprocessed_image
 
--- model_xray/zenml/steps/embedding.py
+++ model_xray/zenml/steps/embedding.py
@@ -4,7 +4,7 @@ import itertools
 from typing_extensions import Annotated
 import numpy as np
 import numpy.typing as npt
-from zenml import ArtifactConfig, get_step_context, step, pipeline
+from zenml import ArtifactConfig, get_step_context, log_artifact_metadata, step, pipeline
 
 from model_xray.configs.models import *
 from model_xray.procedures.embedding_procs import MalBytes, embed_type_map, execute_embedding_proc
@@ -16,7 +16,20 @@ from model_xray.configs.types import COVER_DATA_TYPE
 def embed_payload_into_cover_data_step(
     cover_data: COVER_DATA_TYPE,
     embed_payload_config: EmbedPayloadConfig
-):
+) -> Annotated[
+        COVER_DATA_TYPE,
+        ArtifactConfig(
+            name="stego_data",
+        ),
+    ]:
+
     stego_data = execute_embedding_proc(cover_data=cover_data, embed_payload_config=embed_payload_config)
+
+    log_artifact_metadata(
+        artifact_name="stego_data",
+        metadata={
+            "embed_payload_config": embed_payload_config.model_dump(mode="json"),
+        },
+    )
 
     return stego_data
