d8574a698080ed12c17c6c5cbde8e028084cc126
danigil <dgilkarov33@gmail.com>
2025-03-19 14:53:48 +0200

    avoid importing tf

--- model_xray/configs/types.py
+++ model_xray/configs/types.py
@@ -3,13 +3,13 @@ from typing import Type, Union
 import numpy as np
 import numpy.typing as npt
 
-from torch.nn import Module as torchModel
-from tensorflow.keras import Model as tfkerasModel
-from keras import Model as kerasModel
-from transformers import PreTrainedModel as HFPreTrainedModel
-from transformers import TFPreTrainedModel as HFTFPreTrainedModel
+# from torch.nn import Module as torchModel
+# from tensorflow.keras import Model as tfkerasModel
+# from keras import Model as kerasModel
+# from transformers import PreTrainedModel as HFPreTrainedModel
+# from transformers import TFPreTrainedModel as HFTFPreTrainedModel
 
-from tensorflow.data import Dataset as tfDataset
+# from tensorflow.data import Dataset as tfDataset
 
 # torchModule = Type[torchModule]
 # tfModel = Type[tfModel
@@ -17,6 +17,7 @@ from tensorflow.data import Dataset as tfDataset
 # HFTFPreTrainedModel = Type[HFTFPreTrainedModel
 # kerasModel = Union[kerasModel, tfkerasModel]
 
-DL_MODEL_TYPE = Union[torchModel, kerasModel, tfkerasModel, HFPreTrainedModel, HFTFPreTrainedModel]
+# DL_MODEL_TYPE = Union["torch.nn.Module", kerasModel, tfkerasModel, HFPreTrainedModel, HFTFPreTrainedModel]
+DL_MODEL_TYPE = Union["torch.nn.Module", "keras.Model", "tensorflow.keras.Model", "transformers.PreTrainedModel", "transformers.TFPreTrainedModel"]
 
 COVER_DATA_TYPE = Union[DL_MODEL_TYPE, np.ndarray]
